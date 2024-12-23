6669bdda09d3a6660caa3f6d210ac7434cb590ea
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-12-23 13:34:51 +0000

    modified:   Transfer_learning_for_video.py

--- Transfer_learning_for_video.py
+++ Transfer_learning_for_video.py
@@ -33,3 +33,26 @@ num_frames = 8
 
 output_signature = (tf.TensorSpec(shape = (None, None, None, 3), dtype = tf.float32),
                     tf.TensorSpec(shape = (), dtype = tf.int16))
+
+train_ds = tf.data.Dataset.from_generator(FrameGenerator(subset_paths['train'],num_frames,training = True),
+                                          output_signature = output_signature)
+
+train_ds = train_ds.batch(batch_size)
+
+for frames, labels in train_ds.take(10):
+  print(labels)
+
+print(f'Shape:{frames.shape}')
+print(f'Label:{labels.shape}')
+
+gru = layers.GRU(units= 4 , return_sequences = True , return_state = True )
+
+inputs = tf.random.normal(shape = [1,10,8]) # (batch , seqence ,channels)
+
+result, state = gru(inputs) # Run it all at once
+
+first_half, state = gru(inputs[:,:5,:]) # run the first half , and capture the state
+second_half, _ = gru (inputs[:,5:,:],initial_state = state)  # USa the state to continue where you left off
+
+print(np.allclose(result[:,:5,:],first_half))
+print(np.allclose(result[:,5:,:], second_half))
